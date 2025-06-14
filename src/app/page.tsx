66c4a7a241b3a0517e851f04c845538453c48ba3
takzobye <takzobye@gmail.com>
2025-06-15 05:04:47 +0700

    fix(index): fix link zobyeteam and knw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
         <h1 className="text-xl lg:text-7xl font-black">KHON LABS</h1>
         <div className="mt-2 lg:mt-8 flex flex-col text-xl lg:text-7xl">
           <Link href="https://witx.app" className="hover:underline">WIT X</Link>
-          <Link href="https://zobyeteam" className="hover:underline">ZOBYETEAM</Link>
-          <Link href="https://zobyeteam" className="hover:underline">KNW Badminton Booking</Link>
+          <Link href="https://zobyeteam.com" className="hover:underline">ZOBYETEAM</Link>
+          <Link href="https://knwbadminton.khonlabs.com" className="hover:underline">KNW Badminton Booking</Link>
         </div>
       </div>
     </div>
